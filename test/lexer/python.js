fbf1e0b972b7934af8eff1c59adf400ba467a960
Maxwell-Alexius <destinydragon85614@gmail.com>
2017-09-09 12:54:03 +0800

    Add test for python looping and define method statement

--- test/lexer/python.js
+++ test/lexer/python.js
@@ -27,11 +27,13 @@ describe('Python Lexer', function() {
     '    print("Old")',
     '# => "Young"'
   ].join('\n')
-  let loopingStatement = [
+  let forLoopStatement = [
     's = 0',
     'for i in range(10):',
     '    s += i',
-    '# s = 45',
+    '# s = 45'
+  ].join('\n')
+  let whileLoopStatement = [
     'i = 0',
     'while i < 10:',
     '    i += 1',
@@ -71,9 +73,45 @@ describe('Python Lexer', function() {
       })
     })
 
-    // TODO: Looping Statements
-    // describe('Looping Statements', function() {
-    // })
+    describe('Looping Statements', function() {
+      it('parses the for...in... loop statement', function() {
+        let actualResults = lexPython(forLoopStatement)
+        let expectedResults = [
+          T.default('s'), T.space(), T.operator('='), T.space(), T.default('0'), T.newline(),
+          T.keyword('for'), T.space(), T.default('i'), T.space(), T.keyword('in'), T.space(), T.func('range'), T.leftParentheses(), T.default('10'), T.rightParentheses(), T.colon(), T.newline(),
+          T.space('    '), T.default('s'), T.space(), T.operator('+'), T.operator('='), T.space(), T.default('i'), T.newline(),
+          T.comment('# s = 45')
+        ]
+
+        checkLexedResult(expectedResults, actualResults)
+      })
+
+      it('parses the while loop statement', function() {
+        let actualResults = lexPython(whileLoopStatement)
+        let expectedResults = [
+          T.default('i'), T.space(), T.operator('='), T.space(), T.default('0'), T.newline(),
+          T.keyword('while'), T.space(), T.default('i'), T.space(), T.operator('<'), T.space(), T.default('10'), T.colon(), T.newline(),
+          T.space('    '), T.default('i'), T.space(), T.operator('+'), T.operator('='), T.space(), T.default('1'), T.newline(),
+          T.comment('# i = 10')
+        ]
+
+        checkLexedResult(expectedResults, actualResults)
+      })
+    })
+
+    describe('define method statement', function() {
+      it('parses the define method statement', function() {
+        let actualResults = lexPython(defineMethodStatement)
+        let expectedResults = [
+          T.keyword('def'), T.space(), T.default('hello'), T.leftParentheses(), T.default('name'), T.rightParentheses(), T.colon(), T.newline(),
+          T.space('    '), T.func('print'), T.leftParentheses(), T.string('Hello! '), T.space(), T.operator('+'), T.space(), T.default('name'), T.space(), T.operator('+'), T.space(), T.string(' !'), T.rightParentheses(), T.newline(),
+          T.newline(),
+          T.default('hello'), T.leftParentheses(), T.string('Maxwell'), T.rightParentheses(), T.space(), T.comment('# => "Hello! Maxwell!"')
+        ]
+
+        checkLexedResult(expectedResults, actualResults)
+      })
+    })
   })
 
   // TODO: Expressions
