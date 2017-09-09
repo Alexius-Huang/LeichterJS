5bceee967485fef042aead24c4fdaef3ca74b2e2
Maxwell-Alexius <destinydragon85614@gmail.com>
2017-09-09 19:15:39 +0800

    Add Ruby syntax support in Alpha version

--- build/js/leichter.js
+++ build/js/leichter.js
@@ -77,6 +77,7 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 
 var appendNode = __webpack_require__(1).appendNode;
 var ignitePythonCode = __webpack_require__(2).ignitePythonCode;
+var igniteRubyCode = __webpack_require__(5).igniteRubyCode;
 
 module.exports = function () {
   function Leichter() {
@@ -102,6 +103,8 @@ module.exports = function () {
       switch (this.lang) {
         case 'python':
           ignitePythonCode(this.element);break;
+        case 'ruby':
+          igniteRubyCode(this.element);break;
         default:
           console.warn(this.lang + ' lang currently is unsupported!');
       }
@@ -284,18 +287,11 @@ function lexPython(code) {
       var stringClosing = [lexedString[0], lexedString[0], lexedString[0]].join('');
 
       while (currentChar && currentChar + peekChar + code[charPosition + 2] != stringClosing) {
-        // && false) {
-        // console.log('Current: ' + currentChar)
-        // console.log('Peeked: ' + peekChar)
-        // console.log('lexed: ' + lexedString)
-        // debugger
-        // console.clear()
         lexedString += currentChar;
         readChar();
       }
 
       for (var i = 0; i < 3; i++) {
-
         if (currentChar) {
           lexedString += currentChar;
         } else break;
@@ -519,6 +515,331 @@ function tokenizePython(tokens) {
         if (peekToken && peekToken.type === 'left-parentheses') {
           parseArgumentTokens();
         }
+        break;
+
+      case 'default':
+        if (!isNaN(currentToken.value)) {
+          currentToken.type = 'number';
+        }
+        nextToken();
+        break;
+
+      default:
+        nextToken();
+    }
+  }
+
+  return tokens;
+}
+
+/***/ }),
+/* 5 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.igniteRubyCode = igniteRubyCode;
+var lexRuby = __webpack_require__(6).lexRuby;
+var tokenizeRuby = __webpack_require__(8).tokenizeRuby;
+var className = 'lt';
+
+function igniteRubyCode(element) {
+  var codeString = element.innerText;
+
+  /* Parse the code for the first time into fundamental tokens */
+  var lexedTokens = lexRuby(codeString);
+
+  /* Secondary parse for complex combination syntax feature */
+  var tokens = tokenizeRuby(lexedTokens);
+
+  /* Parsing Ruby code highlighting */
+  element.innerText = '';
+  var _iteratorNormalCompletion = true;
+  var _didIteratorError = false;
+  var _iteratorError = undefined;
+
+  try {
+    for (var _iterator = tokens[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+      var token = _step.value;
+
+      var codeSegment = document.createElement('span');
+      codeSegment.className = className + ' ' + className + '-' + token.type;
+      codeSegment.innerText = token.value;
+      element.appendChild(codeSegment);
+    }
+  } catch (err) {
+    _didIteratorError = true;
+    _iteratorError = err;
+  } finally {
+    try {
+      if (!_iteratorNormalCompletion && _iterator.return) {
+        _iterator.return();
+      }
+    } finally {
+      if (_didIteratorError) {
+        throw _iteratorError;
+      }
+    }
+  }
+}
+
+/***/ }),
+/* 6 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.lexRuby = lexRuby;
+var $keywords = ['alias ', 'and', 'BEGIN', 'begin', 'break', 'case', 'class', 'def', 'defined?', 'do', 'else', 'elsif', 'END', 'end', 'ensure', 'false', 'for', 'if', 'in', 'module', 'next', 'nil', 'not', 'or', 'redo', 'rescue', 'retry', 'return', 'self', 'super', 'then', 'true', 'undef', 'unless', 'until', 'when', 'while', 'yield'];
+var $functions = ['include', 'using', 'public', 'private', 'define_method', 'irb_exit_org', 'default_src_encoding', 'irb_binding', 'sprintf', 'format', 'catch', 'throw', 'loop', 'block_given?', 'trace_var', 'untrace_var', 'at_exit', 'set_trace_func', 'caller', 'caller_locations', 'select', 'test', 'fork', '`', 'sleep', 'gem_original_require', 'respond_to_missing?', 'load', 'exec', 'exit!', 'system', 'spawn', 'abort', 'syscall', 'printf', 'open', 'putc', 'print', 'readline', 'puts', 'p', 'readlines', 'rand', 'gets', 'srand', 'proc', 'lambda', 'gem', 'initialize_copy', 'initialize_clone', 'initialize_dup', 'trap', 'require', 'require_relative', 'autoload', 'autoload?', 'binding', 'local_variables', 'warn', 'raise', 'fail', 'global_variables', '__method__', '__callee__', '__dir__', 'eval', 'iterator?', 'method_missing', 'singleton_method_added', 'singleton_method_removed', 'singleton_method_undefined', 'initialize'];
+
+String.prototype.last = function () {
+  return this[this.length - 1];
+};
+Array.prototype.includes = function (item) {
+  return this.indexOf(item) !== -1;
+};
+
+function lexRuby(code) {
+  var currentChar = code[0];
+  var peekChar = code[1];
+  var charPosition = 0;
+  var lexedValue = '';
+  var tokens = [];
+
+  function readChar() {
+    charPosition++;
+    currentChar = code[charPosition];
+    peekChar = code[charPosition + 1];
+  }
+
+  function readString() {
+    var lexedString = currentChar;
+    readChar();
+
+    /* Single Line String Case */
+    while (currentChar != lexedString[0] && currentChar !== '\n') {
+      lexedString += currentChar;
+      readChar();
+    }
+
+    if (currentChar !== '\n') {
+      /* Current charcacter is the closing string */
+      lexedString += currentChar;
+      readChar();
+    }
+
+    /* Push String Token */
+    tokens.push({ type: 'string', value: lexedString });
+  }
+
+  function readSpace() {
+    var spaces = currentChar;
+    readChar();
+
+    while (currentChar == ' ') {
+      spaces += ' ';
+      readChar();
+    }
+
+    tokens.push({ type: 'space', value: spaces });
+  }
+
+  function readComment() {
+    var commentString = currentChar;
+    readChar();
+
+    while (currentChar !== undefined && currentChar != '\n') {
+      commentString += currentChar;
+      readChar();
+    }
+
+    tokens.push({ type: 'comment', value: commentString });
+  }
+
+  function tokenizeLexedValue() {
+    if (lexedValue) {
+      if ($keywords.includes(lexedValue)) {
+        tokens.push({ type: 'keyword', value: lexedValue });
+      } else if ($functions.includes(lexedValue)) {
+        tokens.push({ type: 'function', value: lexedValue });
+      } else {
+        tokens.push({ type: 'default', value: lexedValue });
+      }
+      lexedValue = '';
+    }
+  }
+
+  while (currentChar !== undefined) {
+    switch (currentChar) {
+      case ' ':
+        tokenizeLexedValue();
+        readSpace();
+        break;
+
+      case '"':
+      case '\'':
+        tokenizeLexedValue();
+        readString();
+        break;
+
+      case '=':
+      case '+':
+      case '-':
+      case '*':
+      case '/':
+      case '>':
+      case '<':
+      case '|':
+      case '&':
+        tokenizeLexedValue();
+        tokens.push({ type: 'operator', value: currentChar });
+        readChar();
+        break;
+
+      case ':':
+        tokenizeLexedValue();
+        tokens.push({ type: 'colon', value: ':' });
+        readChar();
+        break;
+
+      case '[':
+        tokenizeLexedValue();
+        tokens.push({ type: 'left-bracket', value: '[' });
+        readChar();
+        break;
+
+      case ']':
+        tokenizeLexedValue();
+        tokens.push({ type: 'right-bracket', value: ']' });
+        readChar();
+        break;
+
+      case '(':
+        tokenizeLexedValue();
+        tokens.push({ type: 'left-parentheses', value: '(' });
+        readChar();
+        break;
+
+      case ')':
+        tokenizeLexedValue();
+        tokens.push({ type: 'right-parentheses', value: ')' });
+        readChar();
+        break;
+
+      case '{':
+        tokenizeLexedValue();
+        tokens.push({ type: 'left-brace', value: '{' });
+        readChar();
+        break;
+
+      case '}':
+        tokenizeLexedValue();
+        tokens.push({ type: 'right-brace', value: '}' });
+        readChar();
+        break;
+
+      case ',':
+        tokenizeLexedValue();
+        tokens.push({ type: 'comma', value: ',' });
+        readChar();
+        break;
+
+      case ';':
+        tokenizeLexedValue();
+        tokens.push({ type: 'semicolon', value: ':' });
+        readChar();
+        break;
+
+      case '\n':
+        tokenizeLexedValue();
+        tokens.push({ type: 'newline', value: '\n' });
+        readChar();
+        break;
+
+      case '#':
+        readComment();
+        break;
+
+      default:
+        lexedValue += currentChar;
+        readChar();
+    }
+  }
+
+  if (lexedValue) tokenizeLexedValue();
+
+  return tokens;
+}
+
+/***/ }),
+/* 7 */,
+/* 8 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.tokenizeRuby = tokenizeRuby;
+/*
+ * Ruby Tokenizer
+ * 
+ * Basically tokenizer is for the secondary parse of the tokens
+ * in order to parse complex token combinations or types
+ *
+ */
+
+function tokenizeRuby(tokens) {
+  var tokenPosition = 0;
+  var currentToken = tokens[0];
+  var peekToken = tokens[1];
+
+  function nextToken() {
+    tokenPosition++;
+    currentToken = tokens[tokenPosition];
+    peekToken = tokens[tokenPosition + 1];
+  }
+
+  function parseArgumentTokens() {
+    while (currentToken && ['right-parentheses', 'newline', 'semicolon'].indexOf(currentToken.type) === -1) {
+      if (currentToken.type === 'default') {
+        currentToken.type = 'argument';
+      }
+      nextToken();
+    }
+  }
+
+  while (currentToken) {
+    switch (currentToken.type) {
+      case 'keyword':
+        if (currentToken.value === 'def' && peekToken && peekToken.type === 'space') {
+          nextToken();
+          if (peekToken && peekToken.type === 'default') {
+            peekToken.type = 'function-name';
+          }
+        } else if (currentToken.value === 'class' && peekToken && peekToken.type === 'space') {
+          nextToken();
+          if (peekToken && peekToken.type === 'default') {
+            peekToken.type = 'class-name';
+          }
+        }
+        nextToken();
+        break;
+
+      case 'function-name':
+      case 'class-name':
+        parseArgumentTokens();
         break;
 
       case 'default':
